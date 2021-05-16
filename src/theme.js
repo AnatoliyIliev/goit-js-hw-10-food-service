3d1fe0f825cab16084b5219eaeb95f0fb48e4f1b
Anatoliy-Iliev <vekok12@gmail.com>
2021-05-16 21:05:05 +0300

    hw10

--- src/theme.js
+++ src/theme.js
@@ -8,27 +8,36 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const thema = localStorage.getItem('localTheme') || Theme.LIGHT;
+body.classList.add(thema);
+themeEl.checked = thema === Theme.DARK;
+
+// if (!localStorage.localTheme) {
+//     localStorage.setItem('localTheme', Theme.LIGHT);
+// }
+
+// body.classList = localStorage.getItem('localTheme');
 
 
-if (!localStorage.localTheme) {
-    localStorage.setItem('localTheme', Theme.LIGHT);
-}
-
-body.classList = localStorage.getItem('localTheme');
-
-
-if (body.classList.contains(Theme.DARK)) {
-    themeEl.checked = true;
-    // themeEl.prop('checked', true);    
-}
+// if (body.classList.contains(Theme.DARK)) {
+//     themeEl.checked = true;
+//     // themeEl.prop('checked', true);    
+// }
 
 function changeTheme() {
     if (localStorage.getItem('localTheme') === Theme.LIGHT) {
-        body.classList.remove(Theme.LIGHT);
-        body.classList.add(Theme.DARK);
+        removeOrAdd(Theme.LIGHT, Theme.DARK)
+        // body.classList.remove(Theme.LIGHT);
+        // body.classList.add(Theme.DARK);
         return localStorage.setItem('localTheme', Theme.DARK);
-    }    
-    body.classList.remove(Theme.DARK);
-    body.classList.add(Theme.LIGHT);
+    }
+    removeOrAdd(Theme.DARK, Theme.LIGHT)
+    // body.classList.remove(Theme.DARK);
+    // body.classList.add(Theme.LIGHT);
     return localStorage.setItem('localTheme', Theme.LIGHT);
 }
+
+function removeOrAdd(rem, add) {
+    body.classList.remove(rem);
+    body.classList.add(add);
+ }
